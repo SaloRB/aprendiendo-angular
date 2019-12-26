7129425164d507d975468d6940eb78eb818e613c
Salomon Romano <salo_romano@yahoo.com>
2019-12-26 16:53:19 -0600

    Clases y Modelos

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -1,9 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Zapatilla } from "../models/zapatilla";
 
 @Component({
     selector: 'zapatillas',
     templateUrl: './zapatillas.component.html'
 })
-export class ZapatillasComponent {
+export class ZapatillasComponent implements OnInit {
     public titulo: string = "Componente de zapatillas"
+    public zapatillas: Array<Zapatilla>;
+
+    constructor() {
+        this.zapatillas = [
+            new Zapatilla('Nike Airmax', 'Nike', 'Rojo', 190, true),
+            new Zapatilla('Reebok Classic', 'Reebok', 'Blanco', 80, true),
+            new Zapatilla('Nike Runner MD', 'Nike', 'Negro', 60, true),
+            new Zapatilla('Adidas Yeezy', 'Adidas', 'Gris', 180, false)
+        ];
+    }
+
+    ngOnInit() {
+        console.log(this.zapatillas);
+    }
 }
